e21b5800dc46d9a7703b3ab0f1b7a9a2831812f0
CJRS1 <reyes.christian@pucp.pe>
2023-08-24 19:03:48 -0700

    Se hizo que se muestren los servicios

--- amddi_front_del_back/src/components/TablaUsuarios.js
+++ amddi_front_del_back/src/components/TablaUsuarios.js
@@ -11,39 +11,39 @@ export default function Inicio() {
         window.scrollTo(0, 0);
     }, [location]);
 
-    const [usuarios, setUsuarios] = useState([]);
+    const [usuariosConServicios, setUsuariosConServicios] = useState([]);
+
     useEffect(() => {
-        async function fetchUsuarios() {
+        async function fetchUsuariosConServicios() {
             try {
-                console.log('Haciendo llamada a la API a:', 'http://localhost:5000/usuarios');
-                const res = await axios.get('http://localhost:5000/usuarios');
+                console.log('Haciendo llamada a la API a:', 'http://localhost:5000/usuarios_con_servicio');
+                const res = await axios.get('http://localhost:5000/usuarios_con_servicio');
                 console.log(res.data.message);
                 console.log('Response from server:', res.data);
 
                 if (res.data.content && Array.isArray(res.data.content)) {
-                    console.log('Usuarios recibidos:', res.data.content);
-                    setUsuarios(res.data.content);
-                    
+                    console.log('Usuarios con servicios recibidos:', res.data.content);
+                    setUsuariosConServicios(res.data.content);
                 }
             } catch (error) {
-                console.error('Error fetching usuarios:', error);
+                console.error('Error fetching usuarios con servicios:', error);
             }
         }
-    
-        fetchUsuarios();
+
+        fetchUsuariosConServicios();
     }, []);
-    
-    console.log('Usuarios nombres:', usuarios.map(usuario => usuario.nombre));
+
+    console.log('Usuarios con servicios:', usuariosConServicios.map(usuario => usuario.nombre));
     return (
         <div className="container mt-5">
-            <h1>Lista de Usuarios</h1>
+            <h1>Lista de Usuarios con Servicios</h1>
             <table className="table">
                 <thead>
                     <tr>
                         <th>ID</th>
                         <th>Nombre</th>
                         <th>Apellido Paterno</th>
-                        {/* <th>Apellido Materno</th> */}
+                        {/* Agrega más encabezados según tu modelo */}
                         <th>Departamento</th>
                         <th>Carrera</th>
                         <th>Email</th>
@@ -52,16 +52,15 @@ export default function Inicio() {
                         <th>PDF_URL</th>
                         <th>Monto Pagado</th>
                         <th>Monto Total</th>
-                        {/* Agrega más encabezados según tu modelo */}
+                        <th>Servicio</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {usuarios.map(usuario => (
+                    {usuariosConServicios.map(usuario => (
                         <tr key={usuario.id}>
                             <td>{usuario.id}</td>
                             <td>{usuario.nombre}</td>
                             <td>{usuario.apePat}</td>
-                            {/* <td>{usuario.apeMat}</td> */}
                             <td>{usuario.departamento}</td>
                             <td>{usuario.carrera}</td>
                             <td>{usuario.email}</td>
@@ -71,11 +70,19 @@ export default function Inicio() {
                             <td>{usuario.monto_pagado}</td>
                             <td>{usuario.monto_total}</td>
                             {/* Agrega más celdas según tu modelo */}
+                            <td>
+                                <ul>
+                                    {usuario.usuario_servicio.map(usuServ => (
+                                        <li key={usuServ.id}>
+                                            {usuServ.servicio.nombre_servicio}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
             </table>
         </div>
-
     );
 }
